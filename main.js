63ab093ca8a93354765f09ac694c7cf0b53b14b7
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-08-05 09:56:21 -0600

    Complete basic data model and functions for classic game

--- main.js
+++ main.js
@@ -1,89 +1,67 @@
-function createPlayer(nameInput, tokenChoice){
-  var humanPlayer = {
-    name: nameInput, 
-    token: tokenChoice,
-    wins: 0,
-  }
-  return humanPlayer
+var rock = {
+  name: "rock",
+  win: "scissors",
+  loss: "paper",
+}
+var scissors = {
+  name: "scissors",
+  win: "paper", 
+  loss: "rock",
+}
+var paper = {
+  name: "paper",
+  win: "rock",
+  loss: "scissors"
 }
 
-function createGame(){
-  var player1 = createPlayer(nameInput, tokenChoice);
-  var player2 = createPlayer("computer", computerToken);
-  var players = [player1, player2]
-  return players
+var player1 = {};
+
+var player2 = {
+  name: 'Computer',
+  wins: 0
+};
+
+function createPlayer(nameInput){
+  player1 = {
+    name: nameInput, 
+    wins: 0,
+  }
+  return player1
+}
+
+// function createGame(nameInput){
+//   var game = {
+//     player1: createPlayer(nameInput),
+//     player2: createPlayer("computer"),
+//   }
+//   return game
+// }
+
+function getRandomIndex(){
+  var randomIndex = Math.floor(Math.random() * 3);
+  return randomIndex
 }
 
 function computerChoice(){
-  var classicChoice = [rock, paper, scissors];
-  var randomIndex = Math.floor(math.Random() * 3);
-  var cpuChoiceClassic = classicChoice[randomIndex];
+  var classicChoice = ["rock", "paper", "scissors"];
+  var cpuChoiceClassic = classicChoice[getRandomIndex()];
   return cpuChoiceClassic
 }
 
 
+
 function playGame(playerChoice){
-  
-  computerChoice();
-
-  if (playerChoice === "rock" && cpuChoiceClassic === "scissors"){
-    return win(player1)
+  var cpuChoiceClassic = computerChoice()
+  console.log(playerChoice);
+  console.log(cpuChoiceClassic);
+  if (playerChoice.name === cpuChoiceClassic){
+    return 'draw'
   }
-  if (playerChoice === "rock" && cpuChoiceClassic === "paper"){
-    return win(player2)
+  if (playerChoice.win === cpuChoiceClassic){
+    return player1.wins++
   }
-
-
-  if (playerChoice === "paper" && cpuChoiceClassic === "rock"){
-    return win(player1)
-  }
-  if (playerChoice === "paper" && cpuChoiceClassic === "scissors"){
-    return win(player2)
-  }
-
-
-  if (playerChoice === "scissors" && cpuChoiceClassic === "paper"){
-    return win(player1)
-  }
-  if (playerChoice === "scissors" && cpuChoiceClassic === "rock"){
-    return win(player2)
-  }
-
-  if (playerChoice === cpuChoiceClassic){
-    return win(draw)
-  }
-
-}
-
-
-function altfunction(playerChoice){
-  var rock = {
-    win: scissors,
-    loss: paper,
-  }
-  var scissors = {
-    win: paper, 
-    loss: rock,
-  }
-  var paper = {
-    win: rock,
-    loss: scissors
-  }
-  computerChoice();
-  if (playerChoice === cpuChoiceClassic){
-    return win(draw)
-  } else if (playerChoice.win === cpuChoiceClassic){
-    return win(player1)
-  } else if (playerChoice.loss === cpuChoiceClassic){
-    return win(player2)
+  if (playerChoice.loss === cpuChoiceClassic){
+    return player2.wins++
   }
 }
 
-
-function win(player){
-  if (player === draw){
-    return `draw`
-  } else {
-    return player.wins++
-  }
-}
